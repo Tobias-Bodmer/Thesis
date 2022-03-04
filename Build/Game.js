85dcca62be6feb53f7a03f18dd40a89c0adac155
Tobias-Bodmer <tobias.bodmer@hs-furtwangen.de>
2022-03-04 19:13:12 +0100

    Changes on tsconfig and simple GUI for connections

--- Build/Game.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var Game;
-(function (Game) {
-    console.log("hello world");
-})(Game || (Game = {}));
-//# sourceMappingURL=Game.js.map
--- Build/Main.js
+++ Build/Main.js
@@ -1,54 +1,46 @@
-    //#region "Imports"
-    // import ƒ = FudgeCore;
-    // import ƒAid = FudgeAid;
-    // import ƒInterface = FudgeUserInterface;
-    import { FudgeServer } from "../FUDGE/Net/Server/FudgeServer.js";
-    import ƒNet = FudgeNet;
-    //#endregion "Imports"
-
-namespace Game {
+//#region "Imports"
+// import ƒ = FudgeCore;
+// import ƒAid = FudgeAid;
+// import ƒInterface = FudgeUserInterface;
+import { FudgeServer } from "../FUDGE/Net/Server/FudgeServer.js";
+var ƒNet = FudgeNet;
+//#endregion "Imports"
+var Game;
+(function (Game) {
     //#region "DomElements"
-    let portHost = (<HTMLInputElement>document.getElementById("PortHost"));
+    let portHost = document.getElementById("PortHost");
     document.getElementById("Host").addEventListener("click", hostServer, true);
-    let IPConnection = (<HTMLInputElement>document.getElementById("IPConnection"));
-    let PortConnection = (<HTMLInputElement>document.getElementById("PortConnection"));
+    let IPConnection = document.getElementById("IPConnection");
+    let PortConnection = document.getElementById("PortConnection");
     document.getElementById("Connecting").addEventListener("click", conneting, true);
     //#endregion "DomElements"
-
     //#region "PublicVariables"
-
     //#endregion "PublicVariables"
-
     //#region "PrivateVariables"
-
     //#endregion "PrivateVariables"
-    
     //#region "FudgeNetComponent"
     function hostServer() {
         if (!isNaN(+portHost.value)) {
-
             //TODO: learn FudgeNet
             let host = new FudgeServer();
             host.startUp(+portHost.value);
-
             console.log(host.socket);
-        } else {
+        }
+        else {
             alert("Your Port is not a number");
         }
     }
-    
     function conneting() {
         if (!isNaN(+IPConnection.value) && !isNaN(+PortConnection.value)) {
-            
             //TODO: learn FudgeNet
             let client = new ƒNet.FudgeClient();
             client.connectToServer();
-
-            console.log(+IPConnection.value)
+            console.log(+IPConnection.value);
             console.log(IPConnection.value + PortConnection.value);
-        } else {
+        }
+        else {
             alert("Your IP or Port is not a number");
         }
-     }
+    }
     //#endregion "FudgeNetComponent"
-}
+})(Game || (Game = {}));
