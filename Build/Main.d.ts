63795affb3844307aad4dcfe761f04e1b0c0f77a
Tobias-Bodmer <tobias.bodmer@hs-furtwangen.de>
2022-03-15 21:21:08 +0100

    some classes

--- Build/Main.d.ts
+++ Build/Main.d.ts
@@ -1,9 +1,21 @@
 /// <reference path="../FUDGE/Net/Build/Client/FudgeClient.d.ts" />
+/// <reference path="../FUDGE/Net/Build/Server/FudgeServer.d.ts" />
 /// <reference types="../fudge/core/build/fudgecore.js" />
+/// <reference types="../fudge/aid/build/fudgeaid.js" />
+declare namespace Enemy {
+}
+declare namespace Items {
+}
 declare namespace Level {
     class Landscape extends ƒ.Node {
         constructor(_name: string);
     }
 }
 declare namespace Game {
+    export import ƒ = FudgeCore;
+    export import ƒAid = FudgeAid;
+    let viewport: ƒ.Viewport;
+    let graph: ƒ.Node;
+}
+declare namespace Player {
 }
--- Classes/Main.ts
+++ Classes/Main.ts
@@ -2,12 +2,14 @@
 ///<reference types="../FUDGE/Core/Build/FudgeCore.js"/>
 ///<reference types="../FUDGE/Aid/Build/FudgeAid.js"/>
 ///<reference path="../FUDGE/Net/Build/Client/FudgeClient.d.ts"/>
+///<reference path="../FUDGE/Net/Build/Server/FudgeServer.d.ts"/>
 //#endregion "Imports"
 
 namespace Game {
-    import ƒ = FudgeCore;
-    import ƒAid = FudgeAid;
+    export import ƒ = FudgeCore;
+    export import ƒAid = FudgeAid;
     import ƒClient = FudgeNet.FudgeClient;
+    // import ƒServer = FudgeServer;
 
     //#region "DomElements"
     let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("Canvas");
@@ -20,8 +22,8 @@ namespace Game {
     //#endregion "DomElements"
 
     //#region "PublicVariables"
-    let viewport: ƒ.Viewport = new ƒ.Viewport();
-    let graph: ƒ.Node = new ƒ.Node("Graph");
+    export let viewport: ƒ.Viewport = new ƒ.Viewport();
+    export let graph: ƒ.Node = new ƒ.Node("Graph");
     //#endregion "PublicVariables"
 
     //#region "PrivateVariables"
@@ -30,7 +32,17 @@ namespace Game {
 
     //#region "essential"
     function init() {
-        graph.addChild(new Level.Landscape(""));
+        cmpCamera.mtxPivot.translateZ(15);
+
+        cmpCamera.mtxPivot.rotateX(180);
+
+        cmpCamera.mtxPivot.lookAt(ƒ.Vector3.ZERO(), ƒ.Vector3.Y());
+
+        graph.addChild(new ƒAid.Node("blob", ƒ.Matrix4x4.IDENTITY(), new ƒ.Material("mtrCharacter", ƒ.ShaderFlat, new ƒ.CoatColored(ƒ.Color.CSS("withe", 0))), new ƒ.MeshCube()));
+        
+        graph.getChildren()[0].mtxLocal.scale(new ƒ.Vector3(1, 1, 1))
+
+        console.log(graph.getChildren()[0].name);
 
         viewport.initialize("Viewport", graph, cmpCamera, canvas);
 
@@ -40,6 +52,13 @@ namespace Game {
     function draw(): void {
         viewport.draw();
     }
+
+    function update() {
+        draw();
+    }
+
+    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
+    ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 69);
     //#endregion "essential"
 
     //#region "FudgeNetComponent"
@@ -60,7 +79,7 @@ namespace Game {
 
     function conneting() {
         let client = new ƒClient();
-        client.connectToServer();
+        client.connectToServer(IPConnection.value + PortConnection.value);
 
         console.log(+IPConnection.value)
         console.log(IPConnection.value + PortConnection.value);
