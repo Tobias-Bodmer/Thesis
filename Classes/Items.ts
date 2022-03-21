ae5f312c48575b700529eed8386801c168b87de4
MauriceHuchler <maurice.huchler@hs-furtwangen.de>
2022-03-21 17:51:28 +0100

    Update Items.ts
    added basic implementation

--- Classes/Items.ts
+++ Classes/Items.ts
@@ -1,7 +1,18 @@
 namespace Items {
     export class Item {
-        constructor() {
-            
+        public itemName: string;
+        public description: string;
+
+        /**
+ * Creates new Item
+ * @param iName name of the item
+ * @param iDesc description of the item
+ */
+        constructor(iName: string, iDesc: string) {
+            this.itemName = iName;
+            this.description = iDesc;
+
+
         }
     }
 }
