f0de77d110bc66e4f5902ed137534ba6d25ff54b
Tobias-Bodmer <tobias.bodmer@hs-furtwangen.de>
2022-03-22 14:59:29 +0100

    background

--- Classes/Main.ts
+++ Classes/Main.ts
@@ -24,30 +24,49 @@ namespace Game {
     //#endregion "PrivateVariables"
 
     //#region "essential"
-    function init() {
+    async function init() {
         player = new Player.Player("Player1", "11", new Player.Character("Thor,", new Player.Attributes(10, 5, 5)));
         player.addComponent(cmpCamera);
         ƒ.Debug.log(player);
 
-        // let node: ƒ.Node = new ƒ.Node("Quad");
+        let node: ƒ.Node = new ƒ.Node("Quad");
 
-        // let mesh: ƒ.MeshQuad = new ƒ.MeshQuad();
-        // let cmpMesh: ƒ.ComponentMesh = new ƒ.ComponentMesh(mesh);
-        // node.addComponent(cmpMesh);
+        node.addComponent(new ƒ.ComponentTransform());
 
-        // let mtrSolidWhite: ƒ.Material = new ƒ.Material("SolidWhite", ƒ.ShaderFlat, new ƒ.CoatColored(ƒ.Color.CSS("WHITE")));
-        // let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(mtrSolidWhite);
-        // node.addComponent(cmpMaterial);
+        let mesh: ƒ.MeshQuad = new ƒ.MeshQuad();
+        let cmpMesh: ƒ.ComponentMesh = new ƒ.ComponentMesh(mesh);
+        node.addComponent(cmpMesh);
 
-        // graph.appendChild(node);
+        let mtrSolidWhite: ƒ.Material = new ƒ.Material("SolidWhite", ƒ.ShaderFlat, new ƒ.CoatTextured(ƒ.Color.CSS("WHITE"),));
+        let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(mtrSolidWhite);
+        node.addComponent(cmpMaterial);
 
+        let newTxt: ƒ.TextureImage = new ƒ.TextureImage();
+        let newCoat: ƒ.CoatTextured = new ƒ.CoatTextured();
+        let oldComCoat: ƒ.ComponentMaterial = new ƒ.ComponentMaterial();
+        let newMtr: ƒ.Material = new ƒ.Material("mtr", ƒ.ShaderFlatTextured, newCoat);
+        let oldPivot: ƒ.Matrix4x4 = new ƒ.Matrix4x4();
+
+        oldComCoat = node.getComponent(ƒ.ComponentMaterial);
+        oldPivot = node.getComponent(ƒ.ComponentMesh).mtxPivot;
+
+        await newTxt.load("./Resources/Image/gras.png");
+        newCoat.color = ƒ.Color.CSS("WHITE");
+        newCoat.texture = newTxt;
+        oldComCoat.material = newMtr;
+
+        node.cmpTransform.mtxLocal.scale(new ƒ.Vector3(50,50,1));
+        node.cmpTransform.mtxLocal.translateZ(-1);
+
+        graph.addChild(node);
+        
         graph.appendChild(player);
-
+        
         ƒAid.addStandardLightComponents(graph);
-
+        
         cmpCamera.mtxPivot.translateZ(25);
         cmpCamera.mtxPivot.rotateY(180);
-
+        
         ƒ.Debug.log(graph);
 
         viewport.initialize("Viewport", graph, cmpCamera, canvas);
